82fd46f02759a3da7f9d2a569feb0c08ceee7b68
Harry Chopra <harrychopra@gmail.com>
2021-07-22 23:34:46 +0100

    create and use a module

---
+++ 01-basics/01-modules/hello/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
+
+func main() {
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
+
+/*
+For production use, we'd publish the example.com/greetings module from its repository
+(with a module path that reflected its published location), where Go tools could
+find it to download it. For now, because we haven't published the module yet,
+we need to adapt the example.com/hello module so it can find the
+example.com/greetings code on our local file system:
+
+$ go mod edit -replace example.com/greetings=../greetings
+
+Updates go.mod to require the greetings module version, and downloads source code
+into the module cache:
+
+$ go tidy
+*/
