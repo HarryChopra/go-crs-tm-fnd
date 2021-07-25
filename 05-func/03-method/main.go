e5eb27a318c93611ee2b7eb1a555c17c26e14549
Harry Chopra <harrychopra@gmail.com>
2021-07-26 00:53:44 +0100

    initial commit

--- 05-func/03-method/main.go
+++ 05-func/03-method/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	First, Last string
+}
+
+type SecretAgent struct {
+	Person
+	LTK bool
+}
+
+func main() {
+	sa1 := SecretAgent{
+		Person: Person{
+			First: "James",
+			Last:  "Bond",
+		},
+		LTK: true,
+	}
+
+	sa1.speak()
+
+}
+
+// method receiver
+func (p Person) speak() {
+	fmt.Printf("Hi, I am %s %s.\n", p.First, p.Last)
 }
