860ce81f27ffe8119809f2735d60cad922eb69aa
Harry Chopra <harrychopra@gmail.com>
2021-07-26 00:48:59 +0100

    defer keyword

--- 05-func/02-defer/main.go
+++ 05-func/02-defer/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	defer foo()
+	bar()
 }
+
+func foo() {
+	fmt.Println("foo called")
+}
+
+func bar() {
+	fmt.Println("bar called")
+}
+
+/*
+bar called
+foo called
+*/
