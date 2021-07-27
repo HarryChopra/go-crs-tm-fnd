4a327ae6c5aa9fe4751ca654ba4a7baadbd87431
Harry Chopra <harrychopra@gmail.com>
2021-07-27 01:47:17 +0100

    anonymous function

--- 05-func/05-anonymous/main.go
+++ 05-func/05-anonymous/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	func(a, b int) {
+		fmt.Println("The sum is:", a+b)
+	}(2, 3)
+
+	result := func(a, b int) int {
+		return a * b
+	}(2, 3)
+
+	fmt.Println("The product is:", result)
 }
+
+/*
+The sum is: 5
+The product is: 6
+*/
