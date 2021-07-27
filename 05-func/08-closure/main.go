9ca16f064d60cba3504334d77bed2ed374bf8dac
Harry Chopra <harrychopra@gmail.com>
2021-07-27 02:19:37 +0100

    closure

--- 05-func/08-closure/main.go
+++ 05-func/08-closure/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	i := incrementer()
+	fmt.Println("i:", i())
+	fmt.Println("i:", i())
+	fmt.Println("i:", i())
+	fmt.Println("i:", i())
+	fmt.Println("i:", i())
+
+	j := incrementer()
+	fmt.Println("j:", j())
+	fmt.Println("j:", j())
+	fmt.Println("j:", j())
 }
+
+func incrementer() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
+
+/*
+i: 1
+i: 2
+i: 3
+i: 4
+i: 5
+
+j: 1
+j: 2
+j: 3
+*/
