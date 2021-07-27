deaf3bd2d5aba6a4d83654d0f0157bdc28204223
Harry Chopra <harrychopra@gmail.com>
2021-07-27 01:57:08 +0100

    first class citizen

--- 05-func/06-first-class/main.go
+++ 05-func/06-first-class/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(calculate(add, 1, 2, 3, 4, 5))
+	fmt.Println(calculate(multiply, 1, 2, 3, 4, 5))
 
 }
+
+func calculate(fn func(...int) int, x ...int) int {
+	return fn(x...)
+}
+
+func add(x ...int) int {
+	var total int
+	for _, v := range x {
+		total += v
+	}
+	return total
+}
+
+func multiply(x ...int) int {
+	total := 1
+	for _, v := range x {
+		total *= v
+	}
+	return total
+}
+
+/*
+15
+120
+*/
