c4c5ccc5d8ffe5c6c20e8bfdf953cde1f45c8a4d
Harry Chopra <harrychopra@gmail.com>
2021-07-28 03:29:44 +0100

    pointer to struct

--- 04-structs/05-pointer/main.go
+++ 04-structs/05-pointer/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	Name string
+}
+
+func ChangeName1(p Person) {
+	p.Name = "Updated Name"
+}
+
+func ChangeName2(p *Person) {
+	p.Name = "JB"
+	// (*p).Name = "JB"
+}
+
+func main() {
+	p := Person{
+		Name: "James Bond",
+	}
+
+	ChangeName1(p) // pass value
+	fmt.Println(p) // James Bond (Unchanged)
+
+	ChangeName2(&p) // Pass reference
+	fmt.Println(p)  // JB
 }
