e943b07d7a055508b06ab2f93a2bb0703cf973e0
Harry Chopra <harrychopra@gmail.com>
2021-07-28 02:50:28 +0100

    factorial using recursion

--- 05-func/09-recursion/02-factorial/main.go
+++ 05-func/09-recursion/02-factorial/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("factorial of 0 is:", factorial(0))
+	fmt.Println("factorial of 1 is:", factorial(1))
+	fmt.Println("factorial of 2 is:", factorial(2))
+	fmt.Println("factorial of 3 is:", factorial(3))
+	fmt.Println("factorial of 4 is:", factorial(4))
 }
+
+func factorial(x int) int {
+	if x > 1 {
+		return factorial(x-1) * x
+	}
+	return 1
+}
+
+/*
+factorial of 0 is: 1
+factorial of 1 is: 1
+factorial of 2 is: 2
+factorial of 3 is: 6
+factorial of 4 is: 24
+*/
