c9ad478828bde6f2ab2ce439e6b940309fa64f23
Harry Chopra <harrychopra@gmail.com>
2021-07-29 15:28:04 +0100

    json tags

--- 06-application/04-json-tags/main.go
+++ 06-application/04-json-tags/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type person struct {
+	First string `json:"firstName"`
+	Last  string `json:"lastName"`
+	Age   int    `json:"age,omitempty"` // Omit if zero value or nil
+	LTK   bool   `json:"-"`             // Unexported field
+}
+
+func main() {
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		LTK:   true,
+	}
+
+	p2 := person{
+		First: "Miss",
+		LTK:   false,
+	}
+
+	people := []person{p1, p2}
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+	// [{"firstName":"James","lastName":"Bond","age":32},{"firstName":"Miss","lastName":""}]
+	// p2's "Age" and "Last" field are empty, yet, "Last" is exported to json
 }
