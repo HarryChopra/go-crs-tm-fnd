d88dcce25d67e8b12ad0699599d65d90c32db4a6
Harry Chopra <harrychopra@gmail.com>
2021-07-29 15:05:06 +0100

    exported vs unexported field

--- 06-application/03-exported-unexported-fields/main.go
+++ 06-application/03-exported-unexported-fields/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type person struct {
+	First string
+	Last  string
+	age   int // unexported field
+}
+
+func main() {
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		age:   32,
+	}
+	fmt.Printf("%#v\n", p1) // main.person{First:"James", Last:"Bond", age:32}
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs)) // {"First":"James","Last":"Bond"}
 }
