902c94b988d33e0136fad408e9990c3cf19659b1
Harry Chopra <harrychopra@gmail.com>
2021-07-29 14:36:58 +0100

    unmarshal into a slice

--- 06-application/02-unmarshal/main.go
+++ 06-application/02-unmarshal/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type person struct {
+	First, Last string
+	Age         int
 }
+
+func main() {
+	rawJSON := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
+
+	people := []person{}
+	if err := json.Unmarshal([]byte(rawJSON), &people); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%#v\n", people)
+	fmt.Println(people[0].First)
+}
+
+/*
+[]main.person{main.person{First:"James", Last:"Bond", Age:32},
+main.person{First:"Miss", Last:"Moneypenny", Age:27}}
+
+James
+*/
