23c5e5b5e9dd327601e1d95e9e3c029259b175d5
Harry Chopra <harrychopra@gmail.com>
2021-07-30 01:28:31 +0100

    reproduce a race condition

--- 07-concurrency/02-race-condition/main.go
+++ 07-concurrency/02-race-condition/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	var counter int
+	routines := 100
+	var wg sync.WaitGroup
+
+	wg.Add(routines)
+	for i := 0; i < routines; i++ {
+		go func() {
+			v := counter
+			v++
+			time.Sleep(time.Millisecond)
+			counter = v
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter:", counter)
 }
+
+/*
+go run -race main.go
+
+Counter: 7
+Found 1 data race(s)
+*/
