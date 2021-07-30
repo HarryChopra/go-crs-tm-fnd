cd1e16baa4c6f1bfc6c4fb32d28f9bf629968f6e
Harry Chopra <harrychopra@gmail.com>
2021-07-30 20:28:53 +0100

    bcrypt

--- 06-application/08-bcrypt/main.go
+++ 06-application/08-bcrypt/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	hash, err := bcrypt.GenerateFromPassword([]byte(`pwd1234`), bcrypt.MinCost)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+	fmt.Println("Hashed password:", string(hash))
+
+	if err := bcrypt.CompareHashAndPassword(hash, []byte(`pwd1234`)); err != nil {
+		fmt.Println("password doesnt match")
+	} else {
+		fmt.Println("password is a match")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/go-crs-tm-fnd
+
+go 1.18
+
+require golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
