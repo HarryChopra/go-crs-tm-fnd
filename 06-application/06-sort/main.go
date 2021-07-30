c3bcac4c3324286977116b3470633e67dc74d8b0
Harry Chopra <harrychopra@gmail.com>
2021-07-30 18:46:21 +0100

    sort slice of ints and strings

--- 06-application/06-sort/main.go
+++ 06-application/06-sort/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	nums := []int{10, 9, 6, 7, 8, 1, 3, 2, 5, 4}
+	letters := []string{"a", "z", "b", "y", "c", "x"}
+
+	// sort the slice
+	sort.Ints(nums)
+	fmt.Println(nums)
+
+	// reverse the slice
+	sort.SliceStable(nums, func(i, j int) bool {
+		return i > j
+	})
+	fmt.Println(nums)
+
+	sort.Strings(letters)
+	fmt.Println(letters)
 }
