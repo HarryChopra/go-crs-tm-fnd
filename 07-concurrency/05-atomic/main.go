1ce9b8cff9880ef8fd6cf2c6bf511bd5320e18c4
Harry Chopra <harrychopra@gmail.com>
2021-07-31 14:22:54 +0100

    package atomic

--- 07-concurrency/05-atomic/main.go
+++ 07-concurrency/05-atomic/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
+func main() {
+	var counter int64
+	nroutines := 100
+	var wg sync.WaitGroup
+
+	wg.Add(nroutines)
+	for i := 0; i < nroutines; i++ {
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Counter:", counter)
 }
+
+/*
+go run -race main.go
+Counter: 100
+*/
