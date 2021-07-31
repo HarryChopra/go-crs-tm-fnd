88d08709dfb613df62be062817b1aeb710a48a07
Harry Chopra <harrychopra@gmail.com>
2021-08-01 00:02:27 +0100

    range over channels and close

--- 08-channels/04-range-close/main.go
+++ 08-channels/04-range-close/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	c := make(chan int)
+	go send(c)
+	// Iterate over channel and receive its messages
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
+func send(c chan<- int) {
+	for i := 1; i <= 10; i++ {
+		c <- i
+	}
+	// Close the channel after the last message is sent,
+	// or the caller routine will go into a deadlock
+	close(c)
 }
