143a491808e41a1c1d7cece8429ca82247501499
Harry Chopra <harrychopra@gmail.com>
2021-07-31 14:17:21 +0100

    mutex/ rwmutex

--- 07-concurrency/04-mutex/main.go
+++ 07-concurrency/04-mutex/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	var counter int
+	nroutines := 100
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	wg.Add(nroutines)
+	for i := 0; i < nroutines; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			v++
+			time.Sleep(time.Millisecond)
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Counter:", counter)
 }
+
+/*
+go run -race main.go
+Counter: 100
+*/
