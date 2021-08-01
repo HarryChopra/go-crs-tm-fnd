75c94e1da494bbc6ea693bf801cce3386bc30d56
Harry Chopra <harrychopra@gmail.com>
2021-08-01 23:43:40 +0100

    Directional channels and type conversion

--- 08-channels/02-directional/main.go
+++ 08-channels/02-directional/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	c := make(chan int)    // Bidirectional
+	cr := make(<-chan int) // Receive channel
+	cs := make(chan<- int) // Send Channel
+
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", cr)
+	fmt.Printf("%T\n", cs)
+
+	// Can't assign a Directional channel to Bidirectional
+	// c = cr
+	// c = cr
+
+	// Can assign a Bidirectional channel to Directional
+	cr = c
+	cs = c
+
+	// Similarly, Bidirectional to directional type convert:
+	cr1 := (<-chan int)(c)
+	cs1 := (chan<- int)(c)
+
+	fmt.Printf("%T\n", cr1)
+	fmt.Printf("%T\n", cs1)
 }
+
+/*
+chan int
+<-chan int
+chan<- int
+
+<-chan int
+chan<- int
+*/
