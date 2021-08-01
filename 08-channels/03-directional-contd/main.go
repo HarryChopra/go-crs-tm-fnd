04b8d70823c7dd4898dc3872dfbf671467519fdd
Harry Chopra <harrychopra@gmail.com>
2021-08-01 23:51:18 +0100

    using bidirectional channels - type safety

--- 08-channels/03-directional-contd/main.go
+++ 08-channels/03-directional-contd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var wg sync.WaitGroup
+
+func main() {
+	c := make(chan int)
+	wg.Add(1)
+	go foo(c) // Does assign bi-directional to directional
+	bar(c)    // Does assign bi-directional to directional
+	wg.Wait()
 }
+
+// send
+func foo(c chan<- int) {
+	c <- 20
+	wg.Done()
+}
+
+// receive
+func bar(c <-chan int) {
+	fmt.Println(<-c)
+}
+
+/*
+
+20
+*/
