2d780c4cc5143b86066cff23a42810c64f3fa009
Harry Chopra <harrychopra@gmail.com>
2021-08-03 10:28:19 +0100

    create a context

--- 09-context/01-basics/main.go
+++ 09-context/01-basics/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+)
+
+func doSomething(ctx context.Context) {
+	fmt.Println("Doing something!")
+}
+
+func main() {
+	ctx := context.Background()
+	doSomething(ctx)
+}
