8ca1598c1312c16cbefc262e79a2e36ab0447072
Harry Chopra <harrychopra@gmail.com>
2021-08-03 12:45:06 +0100

    c context

--- 09-context/05-with-deadline/main.go
+++ 09-context/05-with-deadline/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func doSomething(ctx context.Context) {
+	deadline := time.Now().Add(time.Duration(200 * time.Millisecond))
+	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
+	printCh := make(chan int)
+	go doAnotherThing(ctx, printCh)
+loop:
+	for i := 1; i <= 3; i++ {
+		select {
+		case <-ctx.Done():
+			break loop
+		case printCh <- i:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+
+	cancelCtx()
+	fmt.Println("doSomething: finished")
+}
+
+func doAnotherThing(ctx context.Context, ch <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("doAnotherThing: err = %s\n", err)
+			}
+			fmt.Println("doAnotherThing: finished")
+			return
+		case num := <-ch:
+			fmt.Printf("doAnotherThing: %d\n", num)
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	doSomething(ctx)
 }
