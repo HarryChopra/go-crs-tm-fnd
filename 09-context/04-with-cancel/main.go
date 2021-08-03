bd49333cf249c325bd17a41d9eba432827a4c35d
Harry Chopra <harrychopra@gmail.com>
2021-08-03 12:13:47 +0100

    cancelling context

--- 09-context/04-with-cancel/main.go
+++ 09-context/04-with-cancel/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func doSomething(ctx context.Context) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+	printCh := make(chan int)
+	go doAnotherThing(ctx, printCh)
+	for i := 1; i <= 3; i++ {
+		printCh <- i
+	}
+	cancelCtx()
+	time.Sleep(100 * time.Millisecond) // Optional: To print rest of output (cancel)
+	fmt.Println("doSomething: finished")
+
+}
+
+func doAnotherThing(ctx context.Context, ch <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("doAnotherThing: err = %q\n", err)
+			}
+			fmt.Println("doAnotherThing: finished")
+			return
+
+		case num := <-ch:
+			fmt.Printf("doAnotherThing: %d\n", num)
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	doSomething(ctx)
 }
