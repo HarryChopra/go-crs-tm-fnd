891f97b758722f7507a10c135369271d428e61c7
Harry Chopra <harrychopra@gmail.com>
2021-08-03 10:34:20 +0100

    context with data

--- 09-context/02-with-value/main.go
+++ 09-context/02-with-value/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+)
 
+func doSomething(ctx context.Context) {
+	fmt.Printf("doSomething: Value of myKey = %s\n", ctx.Value("myKey"))
+}
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "myKey", "myVal")
+	doSomething(ctx)
 }
