f994a90a242b82fa2d9ae89e9833953ed70e21a9
Harry Chopra <harrychopra@gmail.com>
2021-08-03 10:40:18 +0100

    context data is immutable

--- 09-context/03-immutable-values/main.go
+++ 09-context/03-immutable-values/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+)
 
+func doSomething(ctx context.Context) {
+	fmt.Printf("doSomething: Value at myKey = %s\n", ctx.Value("myKey"))
+	// Context's data is immutable. WithValue returns a new context.
+	anotherCtx := context.WithValue(ctx, "myKey", "anotherVal")
+	doAnotherThing(anotherCtx)
+	fmt.Printf("doSomething: Value at myKey = %s\n", ctx.Value("myKey"))
+}
+
+func doAnotherThing(ctx context.Context) {
+	fmt.Printf("doAnotherThing: Value at myKey = %s\n", ctx.Value("myKey"))
+}
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "myKey", "myVal")
+	doSomething(ctx)
 }
